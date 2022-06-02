33361b61f434ce3121c61f8a61e615c83c1cfb1f
Aaron Scheiner <blue@aquarat.co.za>
2022-06-02 16:06:06 +0200

    WIP Fixes

--- src/validate-and-send.ts
+++ src/validate-and-send.ts
@@ -1,6 +1,8 @@
 import sgMail from '@sendgrid/mail';
 import axios from 'axios';
-import { makeError } from './utils';
+import { getGlobalConfig, makeError } from './utils';
+
+const config = getGlobalConfig();
 
 interface Data {
   contactOption: string;
@@ -18,11 +20,25 @@ export const validateAndSend = async (event: { body: Data }) => {
 
   if (success) {
     const response = await sendEmail(contactOption, userName, userOrdered, groupName);
-    console.log(response[0].statusCode);
+
+    if (response) {
+      return {
+        statusCode: 200,
+        headers: config.headers,
+        body: JSON.stringify(
+          {
+            message: makeError('recaptcha validation failed'),
+          },
+          null,
+          2
+        ),
+      };
+    }
   }
 
   return {
     statusCode: 400,
+    headers: config.headers,
     body: JSON.stringify(
       {
         message: makeError('recaptcha validation failed'),
@@ -39,27 +55,35 @@ const validateToken = async (token: string) => {
 };
 
 const sendEmail = async (contactOption: string, userName: string, userOrdered: object, groupName?: string) => {
-  const requiredEnvs = ['SENDGRID_API_KEY', 'API3_RECEIVER_MAIL', 'API3_SENDER_MAIL', 'SENDGRID_TEMPLATE_ID' ];
-  requiredEnvs.forEach(env => {
+  const requiredEnvs = ['SENDGRID_API_KEY', 'API3_RECEIVER_MAIL', 'API3_SENDER_MAIL', 'SENDGRID_TEMPLATE_ID'];
+  requiredEnvs.forEach((env) => {
     if (!process.env[env]) {
       throw new Error(`No ${env} ENV found.`);
     }
   });
-//    { text: string } | { html: string } | { templateId: string } | { content: MailContent[] & { 0: MailContent } });
+
   sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
-  await sgMail.send({
+  const sendMailResult = await sgMail.send({
     to: {
       email: process.env.API3_RECEIVER_MAIL!,
     },
     from: {
       email: process.env.API3_SENDER_MAIL!,
     },
-    templateId: process.env.SENDGRID_TEMPLATE_ID!,
-    dynamicTemplateData: {
-      contactOption,
-      userName,
-      userOrdered,
-      groupName: groupName,
-    },
+    subject: `New Order`,
+    text: JSON.stringify(
+      {
+        contactOption,
+        userName,
+        userOrdered,
+        groupName: groupName,
+      },
+      null,
+      2
+    ),
   });
+
+  console.log(sendMailResult);
+  // TODO
+  return true;
 };
