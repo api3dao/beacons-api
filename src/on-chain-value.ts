4a521e11e26b4a5840a5bfa3f7c2946f5108e5d5
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-06-13 10:14:10 -0300

    added support for dapiName parameter

--- src/on-chain-value.ts
+++ src/on-chain-value.ts
@@ -2,6 +2,7 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import { go } from '@api3/airnode-utilities';
 import { getGlobalConfig, makeError } from './utils';
 import { initDb } from './database';
+import { ethers } from 'ethers';
 
 const config = getGlobalConfig();
 
@@ -34,26 +35,48 @@ export const chainValueDataPoint: APIGatewayProxyHandler = async (event): Promis
     };
   }
 
+  var currentDataFeedId = dataFeedId;
+  if(dapiName && !dataFeedId) {
+    const hashedDapiNameId = ethers.utils.formatBytes32String(dapiName);
+    
+    const operation = async () => db.query(`
+      SELECT event_data->'parsedLog'->'args'->> 1 as "dataFeedId" 
+      FROM dapi_events 
+      WHERE 
+      event_name = 'SetDapiName' AND 
+      event_data->'parsedLog'->'args'->> 0 = $1 
+      ORDER BY block DESC
+      LIMIT 1;
+    `, [hashedDapiNameId]);
+
+    const [err, firstResult] = await go(operation, { timeoutMs: 5_000, retries: 2 });
+    if (err || firstResult === undefined) {
+      return {
+        statusCode: 500,
+        headers: config.headers,
+        body: makeError("An error has occurred while querying the dataFeedId"),
+      };
+    }
+    currentDataFeedId = firstResult.rows[0].dataFeedId;
+  }
+
   const operation = async () => db.query(`
-      SELECT event_data -> 'parsedLog' -> 'args' ->> 1 as data
+      SELECT event_data -> 'parsedLog' -> 'args' ->> 1 as "data"
       FROM dapi_events 
       WHERE 
       chain = $1 AND
       event_data->'parsedLog'->'args'->>0 = $2
       ORDER by block DESC 
       LIMIT 1;
-    `, [chainId, dataFeedId]
+    `, [chainId, currentDataFeedId]
   );
 
   const [err, queryResult] = await go(operation, { timeoutMs: 5_000, retries: 2 });
-  const e = err as Error;
   if (err || queryResult === undefined) {
-    console.error(err);
-    console.error(e.stack);
     return {
       statusCode: 500,
       headers: config.headers,
-      body: makeError("An error has occurred while running the query"),
+      body: makeError("An error has occurred while querying the on-chain-value"),
     };
   }
 
