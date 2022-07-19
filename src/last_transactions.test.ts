ad1b9287743d01eb54fd0f218225cf95115e6c58
Aaron Scheiner <blue@aquarat.co.za>
2022-07-19 16:33:18 +0200

    Add config test fixture

--- src/last_transactions.test.ts
+++ src/last_transactions.test.ts
@@ -5,19 +5,10 @@ import { Client } from 'pg';
 import { DEFAULT_TRANSACTION_COUNT, lastTransactions, lastTransactionsQueryTemplate } from './last-transactions';
 import * as database from './database';
 import * as dapiNames from './dapi-names';
-import * as utils from './utils';
+
+process.env.TELEMETRY_CONFIG = path.join(__dirname, '..', 'test', 'fixtures', 'telemetryConfig.example.json');
 
 describe('handles http queries for latest transactions for single dataFeedIds', () => {
-  beforeEach(() => {
-    jest
-      .spyOn(utils, 'getGlobalConfig')
-      .mockReturnValue(
-        JSON.parse(
-          fs.readFileSync(path.join(__dirname, '..', 'test', 'fixtures', 'telemetryConfig.example.json')).toString()
-        )
-      );
-  });
-
   it('responds to a valid query', async () => {
     const validRows = JSON.parse(
       fs.readFileSync(path.join(__dirname, '..', 'test', 'fixtures', 'last-transactions-example.json')).toString()
@@ -166,16 +157,6 @@ describe('handles http queries for latest transactions for single dataFeedIds', 
 });
 
 describe('handles http queries for latest transactions for dAPIs', () => {
-  beforeEach(() => {
-    jest
-      .spyOn(utils, 'getGlobalConfig')
-      .mockReturnValue(
-        JSON.parse(
-          fs.readFileSync(path.join(__dirname, '..', 'test', 'fixtures', 'telemetryConfig.example.json')).toString()
-        )
-      );
-  });
-
   it('responds to a valid query', async () => {
     const validDataFeedIdRows = JSON.parse(
       fs.readFileSync(path.join(__dirname, '..', 'test', 'fixtures', 'last-transactions-example.json')).toString()
